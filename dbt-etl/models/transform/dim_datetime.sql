d04e0754172968599c1fd2bba2a7393eea1069ae
AdnaneKassim <adnanekassim@gmail.com>
2025-12-08 06:13:59 +0100

    update datetime.sql

--- dbt-etl/models/transform/dim_datetime.sql
+++ dbt-etl/models/transform/dim_datetime.sql
@@ -6,23 +6,23 @@ WITH datetime_cte AS (
     CASE
       WHEN LENGTH(CAST(InvoiceDate AS STRING)) = 16 THEN
         -- Date format: "DD/MM/YYYY HH:MM"
-        PARSE_DATETIME('%m/%d/%Y %H:%M', InvoiceDate)
+        PARSE_DATETIME('%m/%d/%Y %H:%M', CAST(InvoiceDate AS STRING))
       WHEN LENGTH(CAST(InvoiceDate AS STRING)) <= 14 THEN
         -- Date format: "MM/DD/YY HH:MM"
-        PARSE_DATETIME('%m/%d/%y %H:%M', InvoiceDate)
+        PARSE_DATETIME('%m/%d/%y %H:%M', CAST(InvoiceDate AS STRING))
       ELSE
         NULL
-    END AS date_part,
+    END AS date_part
   FROM {{ source('retail_dsy', 'raw_invoice') }}
   WHERE InvoiceDate IS NOT NULL
 )
 SELECT
   datetime_id,
-  date_part as datetime,
+  date_part AS datetime,
   EXTRACT(YEAR FROM date_part) AS year,
   EXTRACT(MONTH FROM date_part) AS month,
   EXTRACT(DAY FROM date_part) AS day,
   EXTRACT(HOUR FROM date_part) AS hour,
   EXTRACT(MINUTE FROM date_part) AS minute,
   EXTRACT(DAYOFWEEK FROM date_part) AS weekday
-FROM datetime_cte
+FROM datetime_cte;
